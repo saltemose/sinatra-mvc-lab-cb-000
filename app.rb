8414f55bdefb647c7806614151245ef860c12998
Sean Altemose <sean@seanaltemose.com>
2018-06-27 14:24:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @piglatin = Piglatinizer.new.piglatinize(params[:user_input])
+    erb :results
+  end
+
 end
