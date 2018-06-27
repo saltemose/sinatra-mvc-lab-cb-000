c69ea6ce659a56eb3071f3e78f6c5bd793210215
Sean Altemose <sean@seanaltemose.com>
2018-06-27 14:25:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,34 +1,24 @@
 
 class PigLatinizer
 
-  def piglatinize(input_str)
-    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-    puts x
-    x
-  end
+  def vowel?(char)
+  "aeiouAEIOU".include?(char)
+end
 
-  private
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-  end
-
-  def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
+def piglatinize_word(string)
+  words = string.split
+  words.collect do |word|
+    if vowel?(word[0])
+      word += "way"
     else
-      word = word.slice(1..-1) + word.slice(0)
+      until vowel?(word[0]) do
+        word = word[1..-1] + word[0]
+      end
+      word = word + "ay"
     end
-    word << "ay"
-  end
+  end.join
+end
 
   def piglatinize(user_input)
     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
